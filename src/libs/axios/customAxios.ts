401a6ab0c5e1e48df7fa8dd388764b634a595378
전민찬 <chan2bo2@dgsw.hs.kr>
2025-04-03 14:55:06 +0900

    feat :: customAxios *프로젝트 clone시에 config폴더만들어주세요

---
+++ src/libs/axios/customAxios.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosRequestConfig } from 'axios'
+import CONFIG from '@/config/config.json'
+import {
+  ACCESS_TOKEN_KEY,
+  REQUEST_TOKEN_KEY,
+} from '@/constants/token/token.constants'
+import token from '../token/tokens'
+import requestInterceptor from './requestInterceptor'
+import ResponseHandler from './responseHandler'
+
+const axiosRequestConfig: AxiosRequestConfig = {
+  baseURL: CONFIG.SERVER,
+  withCredentials: true,
+  headers: {
+    [REQUEST_TOKEN_KEY]: `Bearer ${token.getToken(ACCESS_TOKEN_KEY)}`,
+  },
+}
+
+const guessMeWhatAxios = axios.create(axiosRequestConfig)
+guessMeWhatAxios.interceptors.request.use(requestInterceptor as any, (err) =>
+  Promise.reject(err)
+)
+guessMeWhatAxios.interceptors.response.use((res) => res, ResponseHandler)
+
+export default guessMeWhatAxios
+
+export const setAccessToken = (token: string) => {
+  guessMeWhatAxios.defaults.headers[REQUEST_TOKEN_KEY] = `Bearer ${token}`
+}
